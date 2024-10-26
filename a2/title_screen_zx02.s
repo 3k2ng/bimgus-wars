398d4e5618842d180a02c328cdd70870de8b5947
jukmifgguggh1337 <spamandjunkmail9@gmail.com>
2024-10-25 19:46:58 -0600

    import zx02

---
+++ a2/title_screen_zx02.s
@@ -0,0 +1,36 @@
+; title_screen.s
+;
+; displaying the title screen
+; game name: BIMGUS WARS
+; team name: UR MOM
+; year: 2024
+; title picture: 1 green tank facing 3 red tank
+
+        processor 6502
+
+CHARACTER_RAM = $1c00
+
+SCREEN_RAM = $1e00
+; screen_ram split into 2 bytes
+SCRB0 = $00
+SCRB1 = $1e
+
+COLOR_RAM = $9600
+; color_ram split into 2 bytes
+CORB0 = $00
+CORB1 = $96
+
+	org $1001
+
+; BASIC stub 
+        dc.w nextstmt
+        dc.w 10
+        dc.b $9e, [start]d, 0
+nextstmt
+        dc.w 0
+
+; main
+start
+        rts
+
+        include "./data/zx02-optim.asm"
