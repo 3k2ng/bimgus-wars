a93f645cde820c0948045cb3607d8687ddf62a6d
ericrenno <rennoeric@gmail.com>
2024-10-30 18:03:20 -0600

    Closest working version

--- a2/title_screen_ai.s
+++ a2/title_screen_ai.s
@@ -1,11 +1,3 @@
-; title_screen.s
-;
-; displaying the title screen
-; game name: BIMGUS WARS
-; team name: UR MOM
-; year: 2024
-; title picture: 1 green tank facing 3 red tank
-
 	processor 6502
 
 CHARACTER_RAM = $1c00
@@ -20,14 +12,11 @@ COLOR_RAM = $9600
 CORB0 = $00
 CORB1 = $96
 
-; zero page variable position
-; decoding subroutine
-; decoded data location (moving)
-DECODED_DATA_LOC = $00 ; 2 bytes
-; encoded data location (moving)
-ENCODED_DATA_LOC = $02 ; 2 bytes
-; where to read when reading buffer data (moving)
-BACKREAD_DATA_LOC = $04 ; 2 bytes
+; zero page variables
+VALUE = $00       ; Current value to repeat
+COUNT = $01       ; How many times to repeat
+DEST_PTR = $02    ; 2 bytes - destination pointer
+SRC_PTR = $04     ; 2 bytes - source pointer
 
 	org $1001
 
@@ -48,112 +37,97 @@ start
         lda #$0b
         sta $900f
 
-; copy custom character to character ram
-        ldx #0
-ccr_loop
-        lda uncompressed_char_set,x
-        sta CHARACTER_RAM,x
-        inx
-        cpx TITLE_CHAR_SET_SIZE
-        bne ccr_loop
+; copy custom character to character ram using RLE decoding
+        lda #<uncompressed_char_set
+        sta SRC_PTR
+        lda #>uncompressed_char_set
+        sta SRC_PTR+1
+        
+        lda #<CHARACTER_RAM
+        sta DEST_PTR
+        lda #>CHARACTER_RAM
+        sta DEST_PTR+1
+        
+        jsr decode_rle
 
-ccr2_loop
-
-; decode data at hmap to screen ram
+; decode screen data
+        lda #<hcc_char_map
+        sta SRC_PTR
+        lda #>hcc_char_map
+        sta SRC_PTR+1
+        
         lda #SCRB0
-        sta DECODED_DATA_LOC
+        sta DEST_PTR
         lda #SCRB1
-        sta DECODED_DATA_LOC+1
-        lda hmap_loc
-        sta ENCODED_DATA_LOC
-        lda hmap_loc+1
-        sta ENCODED_DATA_LOC+1
-        jsr hcc_decode
+        sta DEST_PTR+1
+        
+        jsr decode_rle
 
-; decode data at hcm to color ram
+; decode color data
+        lda #<hcc_color_map
+        sta SRC_PTR
+        lda #>hcc_color_map
+        sta SRC_PTR+1
+        
         lda #CORB0
-        sta DECODED_DATA_LOC
+        sta DEST_PTR
         lda #CORB1
-        sta DECODED_DATA_LOC+1
-        lda hcm_loc
-        sta ENCODED_DATA_LOC
-        lda hcm_loc+1
-        sta ENCODED_DATA_LOC+1
-        jsr hcc_decode
+        sta DEST_PTR+1
+        
+        jsr decode_rle
 
 inf_loop
         jmp inf_loop
 
-; decoding subroutine
-hcc_decode
-decode_loop
-        ldy #00
-        lda (ENCODED_DATA_LOC),y ; y = 0
+; RLE decoding subroutine
+decode_rle
+        ldy #0
+.decode_loop
+        ; Get the value
+        lda (SRC_PTR),y
+        sta VALUE
+        
+        ; Move to next byte
+        inc SRC_PTR
+        bne .skip1
+        inc SRC_PTR+1
+.skip1
+
+        ; Get the count
+        lda (SRC_PTR),y
+        sta COUNT
+        
+        ; Move to next byte
+        inc SRC_PTR
+        bne .skip2
+        inc SRC_PTR+1
+.skip2
+
+        ; Check if we're done (count = $7f)
+        lda COUNT
         cmp #$7f
-        beq decode_done
-        cmp #$7e
-        beq backread_start
+        beq .done
 
-        sta (DECODED_DATA_LOC),y ; y = 0
-
-        inc ENCODED_DATA_LOC
-        bne sc0
-        inc ENCODED_DATA_LOC+1
-sc0
-
-        inc DECODED_DATA_LOC
-        bne sc1
-        inc DECODED_DATA_LOC+1
-sc1
-
-        jmp decode_loop
-decode_done
-        jmp decode_exit
-
-backread_start
-        clc
-        lda DECODED_DATA_LOC
-        adc #01
-        iny ; y = 1
-        sbc (ENCODED_DATA_LOC),y
-        sta BACKREAD_DATA_LOC
-        lda DECODED_DATA_LOC+1
-        sbc #00
-        sta BACKREAD_DATA_LOC+1
-
-        iny ; y = 2
-        lda (ENCODED_DATA_LOC),y
-        tax
-
-        ldy #00
-backread_loop
-        lda (BACKREAD_DATA_LOC),y ; y = 0
-        sta (DECODED_DATA_LOC),y ; y = 0
-
-        inc BACKREAD_DATA_LOC
-        bne sc2
-        inc BACKREAD_DATA_LOC+1
-sc2
-
-        inc DECODED_DATA_LOC
-        bne sc3
-        inc DECODED_DATA_LOC+1
-sc3
-
+        ; Write the value COUNT times
+        ldx COUNT
+.write_loop
+        lda VALUE
+        ldy #0
+        sta (DEST_PTR),y
+        
+        ; Increment destination
+        inc DEST_PTR
+        bne .skip3
+        inc DEST_PTR+1
+.skip3
+        
         dex
-        bne backread_loop ; if x == 0
+        bne .write_loop
+        
+        jmp .decode_loop
 
-backread_exit
-        clc
-        lda ENCODED_DATA_LOC
-        adc #03
-        sta ENCODED_DATA_LOC
-        lda ENCODED_DATA_LOC+1
-        adc #00
-        sta ENCODED_DATA_LOC+1
-        jmp decode_loop
-
-decode_exit
+.done
         rts
 
-        include "./data/hcc_data.s"
+        ;include "./data/hcc_data.s"
+        include "./data/rle_data.s"
