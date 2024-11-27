32d9221289fe942be7600e4ab29b685944b0f9b0
jukmifgguggh1337 <spamandjunkmail9@gmail.com>
2024-11-26 18:15:59 -0700

    movement sfx

--- project/sound_effects.s
+++ project/sound_effects.s
@@ -18,22 +18,13 @@ FINALP  = 195                   ; final pitch of player explosion
 STARTE  = 215                   ; initial pitch of enemy explosion
 INCE    = 2                     ; rate of increase
 FINALE  = 245                   ; final pitch of enemy explosion
+STARTBV = 8                     ; initial velocity of bullet
 STARTB  = 245                   ; initial pitch of bullet
 FINALB  = 237                   ; final pitch of bullet
 STARTR  = 225                   ; initial pitch of rock
 FINALR  = 217                   ; final pitch of rock
+STARTM  = 215                   ; initial pitch of move
 
-
-; generic cleanup
-exit_sound_effect
-        lda #0                  ; shut off the speaker!
-        ; sta SV
-        ; sta SB                  ; reset the pitches in the four channels
-        ; sta SA
-        ; sta SS
-        ; sta SN
-        ; ldx #0                  ; reset X offset
-        rts
 
 ; player explosion sound effect
 playerExplosion
@@ -109,6 +100,16 @@ jiffyE
 ; move on to the next velocity value
         jmp enemyExplosionNext  ; restart main loop
 
+; generic cleanup
+exit_sound_effect
+        lda #0                  ; shut off the speaker!
+        ; sta SV
+        ; sta SB                  ; reset the pitches in the four channels
+        ; sta SA
+        ; sta SS
+        ; sta SN
+        ; ldx #0                  ; reset X offset
+        rts
 
 ; shoot bullet sound effect
 sfx_bullet
@@ -117,7 +118,7 @@ sfx_bullet
         ; sta SN
         jsr reset_pitches
         ldy #STARTB             ; load the first pitch into the Y register
-        lda velocities          ; load a note velocity (just use the first one from the explosion velocities)
+        lda #STARTBV            ; load the initial note velocity
         sta SV                  ; set speaker to velocity
 ; inner loop (decreasing pitch)
 innerB
@@ -168,6 +169,36 @@ jiffyR
         jmp exit_sound_effect   ; restart main loop
 
 
+; player movement sound effect
+player_movement
+        ; lda #0
+        ; sta SB
+        ; sta SA
+        ; sta SS
+        jsr reset_pitches
+        ldx #0
+        ldy #STARTM             ; load the first pitch into the Y register
+        sty SN                  ; write the note to the noise channel
+player_movement_next
+        lda mov_velocities,X    ; get the current note velocity
+        sta SV                  ; set speakers to volume in accumulator
+; check if we should exit on this note
+        beq exit_sound_effect   ; if volume is 0, exit the main loop
+; we should not exit on this note
+        inx                     ; move on
+; set up the jiffy waiting loop
+        ldy JC                  ; load jiffy clock into Y register
+        iny                     ; Y register now stores the desired end time (one jiffy away)
+; wait one jiffy
+jiffyM
+        cpy JC
+        bne jiffyM
+; move on to the next velocity value
+        jmp player_movement_next ; restart main loop
+
+
 ; define explosion velocities
 velocities
         dc  9, 6, 3, 1, 0
+mov_velocities
+        dc  3, 6, 3, 0
