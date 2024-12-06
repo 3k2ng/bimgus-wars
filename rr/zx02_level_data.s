083bca4387a405c2a538fe110ad3bce3d4aaaf3b
jukmifgguggh1337 <spamandjunkmail9@gmail.com>
2024-12-05 23:34:32 -0700

    third level "YOU WIN"

--- rr/zx02_level_data.s
+++ rr/zx02_level_data.s
@@ -2,7 +2,10 @@ level_1_data
 	incbin "./data/level_1_data.zx02"
 level_2_data
 	incbin "./data/level_2_data.zx02"
+level_3_data
+	incbin "./data/level_3_data.zx02"
 level_data_table
 	dc.w level_1_data
 	dc.w level_2_data
+	dc.w level_3_data
 	dc.w 0
