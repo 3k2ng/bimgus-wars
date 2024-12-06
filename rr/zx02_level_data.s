6d6cd7a6cb86aae12372e64a628817634fcbeb23
jukmifgguggh1337 <spamandjunkmail9@gmail.com>
2024-12-05 23:52:33 -0700

    fourth level

--- rr/zx02_level_data.s
+++ rr/zx02_level_data.s
@@ -4,8 +4,11 @@ level_2_data
 	incbin "./data/level_2_data.zx02"
 level_3_data
 	incbin "./data/level_3_data.zx02"
+level_4_data
+	incbin "./data/level_4_data.zx02"
 level_data_table
 	dc.w level_1_data
 	dc.w level_2_data
 	dc.w level_3_data
+	dc.w level_4_data
 	dc.w 0
