80ca7eef2800c58a7277162ff92341125ac49531
jukmifgguggh1337 <spamandjunkmail9@gmail.com>
2024-12-06 02:40:22 -0700

    level 5

--- rr/zx02_level_data.s
+++ rr/zx02_level_data.s
@@ -6,9 +6,12 @@ level_3_data
 	incbin "./data/level_3_data.zx02"
 level_4_data
 	incbin "./data/level_4_data.zx02"
+level_5_data
+	incbin "./data/level_5_data.zx02"
 level_data_table
 	dc.w level_1_data
 	dc.w level_2_data
 	dc.w level_3_data
 	dc.w level_4_data
+	dc.w level_5_data
 	dc.w 0
